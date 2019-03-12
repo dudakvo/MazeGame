984bf20d36dffc435aae9980563a80c65fdeb6cf
dudakvo <dudakvo@gmail.com>
2019-03-12 12:06:26 +0200

    v1.0

---
+++ def.h
@@ -0,0 +1,10 @@
+const int cMazeColumns = 12;
+const int cMazeRows = 8;
+
+const char cEmptySymbol = ' ';
+const char cWallSymbol = 'X';
+const char cCharacterSymbol = '@';
+const char cExitSymbol = '#';
+const char cKeySymbol = 'K';
+
+using Maze = std::array<std::array<char, cMazeColumns>, cMazeRows>;
